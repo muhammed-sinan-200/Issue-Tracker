d875a5b441943803b9e38c2e610988a390edfb3f
Sinan404 <mhdsinanat20@gmail.com>
2026-06-07 21:03:07 +0530

    debug users service error

--- backend/src/modules/users/controllers/user.controller.ts
+++ backend/src/modules/users/controllers/user.controller.ts
@@ -3,14 +3,15 @@ import * as userService from "../services/user.service";
 
 export async function getAllUsers(_req: Request, res: Response) {
   try {
-    const users = await userService.getAllUsers();
     console.log("GET USERS HIT");
+    const users = await userService.getAllUsers();
     return res.status(200).json({
       success: true,
       message: "Users fetched successfully",
       data: users,
     });
-  } catch {
+  } catch (err){
+    console.log("Error in Users:", err);
     return res.status(500).json({
       success: false,
       message: "Something went wrong",
